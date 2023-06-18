c7bad6752b2cc37c1e6cf1992749f322f235c880
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-18 17:03:54 +0300

    add filtering logic to skill scales & fix images

--- src/widgets/SkillScales/consts/skill-scales.ts
+++ src/widgets/SkillScales/consts/skill-scales.ts
@@ -1,11 +1,11 @@
 import { TScales } from "../../../features/SnakeScale/lib/types"
-import jsLogo from "../lib/assets/js-logo.png"
+import jsLogo from "../lib/assets/js-logo.svg"
 import nestjsLogo from "../lib/assets/nestjs-logo.svg"
-import nextjsLogo from "../lib/assets/nextjs-logo.png"
-import reactLogo from "../lib/assets/react-logo.png"
-import reduxLogo from "../lib/assets/redux-logo.png"
-import scssLogo from "../lib/assets/scss-logo.png"
-import tsLogo from "../lib/assets/ts-logo.png"
+import nextjsLogo from "../lib/assets/nextjs-logo.svg"
+import reactLogo from "../lib/assets/react-logo.svg"
+import reduxLogo from "../lib/assets/redux-logo.svg"
+import scssLogo from "../lib/assets/scss-logo.svg"
+import tsLogo from "../lib/assets/ts-logo.svg"
 
 export const SKILL_SCALES: TScales = [
   {
@@ -43,4 +43,7 @@ export const SKILL_SCALES: TScales = [
     description: "",
     image: nestjsLogo,
   },
-]
+].map((skill, index) => ({
+  ...skill,
+  id: `${index}`,
+}))
--- src/widgets/SkillScales/ui/SkillScales/SkillScales.tsx
+++ src/widgets/SkillScales/ui/SkillScales/SkillScales.tsx
@@ -1,10 +1,16 @@
 import { FilterBar } from "../../../../features/FilterBar/ui"
 import { SnakeScale } from "../../../../features/SnakeScale/ui"
+import { useProjectFiltersContext } from "../../../../shared/lib/hooks"
 import { SKILL_SCALES } from "../../consts"
 import styles from "./SkillScales.module.scss"
 import { FC } from "react"
 
 export const SkillScales: FC = () => {
+  const filtersContext = useProjectFiltersContext()
+
+  const technologies = filtersContext?.filters?.technologies
+  const setFilters = filtersContext?.setFilters
+
   function handleOnFilterChange(payload: string[]) {
     console.log(`on filter change ${payload}`)
   }
@@ -12,13 +18,29 @@ export const SkillScales: FC = () => {
   return (
     <div>
       <div className={styles["skill-scales"]}>
-        <SnakeScale scales={SKILL_SCALES} />
+        <SnakeScale
+          scales={SKILL_SCALES}
+          onScaleClick={(scale) => {
+            const newTechnologies = Array.from(new Set([...(technologies || []), scale.title]))
+
+            setFilters?.((filters) => {
+              return {
+                ...filters,
+                technologies: newTechnologies,
+              }
+            })
+          }}
+        />
       </div>
 
       <footer>
         <FilterBar
           onChange={handleOnFilterChange}
-          tags={[]}
+          tags={
+            technologies?.map((technology) => ({
+              value: technology,
+            })) || []
+          }
         />
       </footer>
     </div>
