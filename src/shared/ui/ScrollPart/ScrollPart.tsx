b62be9b6e86d0d6dc7006adf5664fcf0c4632452
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-23 14:15:00 +0300

    add scroll part component

---
+++ src/shared/ui/ScrollPart/ScrollPart.tsx
@@ -0,0 +1,8 @@
+import styles from "./ScrollPart.module.scss"
+import { FC, PropsWithChildren } from "react"
+
+type ScrollPartProps = PropsWithChildren<{}>
+
+export const ScrollPart: FC<ScrollPartProps> = ({ children }) => {
+  return <>{children}</>
+}
---
+++ src/shared/ui/ScrollPart/index.tsx
@@ -0,0 +1,1 @@
+export { ScrollPart } from "./ScrollPart"
