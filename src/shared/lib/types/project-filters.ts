25d179ab8ec9476074d91ffce3969f1b3b74ab4f
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-23 14:16:01 +0300

    lint

--- src/shared/lib/types/project-filters.ts
+++ src/shared/lib/types/project-filters.ts
@@ -1,3 +1,3 @@
 import { TProject } from "../../../widgets/ProjectList/lib/types"
 
-export type TProjectFilters = Partial<Pick<TProject, 'technologies'>>
+export type TProjectFilters = Partial<Pick<TProject, "technologies">>
