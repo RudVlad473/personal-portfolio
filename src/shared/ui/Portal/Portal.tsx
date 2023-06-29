372dc46cfb48e934c4562d7c9d0a5798e4c6ac81
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-06-29 13:53:54 +0300

    lint problems

--- src/shared/ui/Portal/Portal.tsx
+++ src/shared/ui/Portal/Portal.tsx
@@ -2,7 +2,7 @@ import styles from "./Portal.module.scss"
 import { FC, PropsWithChildren } from "react"
 import { createPortal } from "react-dom"
 
-type PortalProps = PropsWithChildren<{}>
+type PortalProps = PropsWithChildren
 
 export const Portal: FC<PortalProps> = ({ children }) => {
   return <>{createPortal(children, document.body)}</>
--- src/shared/ui/ScrollPart/ScrollPart.tsx
+++ src/shared/ui/ScrollPart/ScrollPart.tsx
@@ -1,7 +1,7 @@
 import styles from "./ScrollPart.module.scss"
 import { FC, PropsWithChildren } from "react"
 
-type ScrollPartProps = PropsWithChildren<{}>
+type ScrollPartProps = PropsWithChildren
 
 export const ScrollPart: FC<ScrollPartProps> = ({ children }) => {
   return <>{children}</>
